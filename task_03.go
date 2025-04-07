7d5d87f0c0f309d7c84a3745d7d58e6bf4b97246
Осенчаков Роман <rongo@yandex.ru>
2025-04-07 13:27:41 +0300

    Add task 03

---
+++ task_03.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("=============================================")
+	fmt.Println("Задача 3. Генератор функции для вычисления суммы чисел от 1 до N")
+	fmt.Println("=============================================")
+	//
+	summator := agregator(9) // Получаем функцию-сумматор с ограничением не более 9
+	//
+	//fmt.Printf("Сумма до вычислений: %d\n", sum)
+	for i := 1; i < 12; i++ {
+		fmt.Printf(" = %d\n", summator())
+	}
+	//fmt.Printf("Сумма после вычислений: %d\n", sum)
+}
+
+// Функция agregator принимает на вход значение-ограничитель
+// и возвращает анонимную функцию по суммированию.
+func agregator(max int) func() int {
+	sum := 0
+	current := 0
+	return func() int {
+		current++
+		fmt.Printf("%2d + %2d", sum, current)
+		if current <= max {
+			sum += current
+		}
+		return sum
+	}
+}
